f0128a396b6bd8e85d404b7526d7b15a960b1d7c
Bartek Kubik <kbartek993@gmail.com>
2022-10-24 21:13:52 +0200

    first page

---
+++ main.js
@@ -0,0 +1,8 @@
+const firstname = 'Bartek';
+const age = 20;
+
+alert('JS działa');
+console.log ('js dziala w consoli');
+console.log (firstname)
+console.log (age)
+console.log (`cześć nazywam sie ${firstname} i mam ${age} lat.`);
